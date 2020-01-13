e704f8a285b0eb8ab251b28fc77856f416b99684
Litl Toast <joshuafhiggins@gmail.com>
2020-01-13 08:25:50 -0500

    Moving Very Important Thing From Backup

---
+++ Toasty Engine V0.0.1/src/me/toast/engine/WindowImage.java
@@ -0,0 +1,91 @@
+public class WindowImage {
+  public static void setIcon(String path, long Window) throws Exception {
+  IntBuffer w = memAllocInt(1);
+            IntBuffer h = memAllocInt(1);
+            IntBuffer comp = memAllocInt(1);
+
+            // Icons
+            {
+                ByteBuffer icon16;
+                ByteBuffer icon32;
+                try {
+                    icon16 = ioResourceToByteBuffer(path, 2048);
+                    icon32 = ioResourceToByteBuffer(path, 4096);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+                try (GLFWImage.Buffer icons = GLFWImage.malloc(2)) {
+                    ByteBuffer pixels16 = STBImage.stbi_load_from_memory(icon16, w, h, comp, 4);
+                    icons
+                            .position(0)
+                            .width(w.get(0))
+                            .height(h.get(0))
+                            .pixels(pixels16);
+
+                    ByteBuffer pixels32 = STBImage.stbi_load_from_memory(icon32, w, h, comp, 4);
+                    icons
+                            .position(1)
+                            .width(w.get(0))
+                            .height(h.get(0))
+                            .pixels(pixels32);
+
+                    icons.position(0);
+                    glfwSetWindowIcon(Window, icons);
+
+                    STBImage.stbi_image_free(pixels32);
+                    STBImage.stbi_image_free(pixels16);
+                }
+            }
+
+            memFree(comp);
+            memFree(h);
+            memFree(w);
+
+        }
+
+        private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity) {
+            ByteBuffer newBuffer = BufferUtils.createByteBuffer(newCapacity);
+            buffer.flip();
+            newBuffer.put(buffer);
+            return newBuffer;
+        }
+
+        /**
+         * Reads the specified resource and returns the raw data as a ByteBuffer.
+         *
+         * @param resource   the resource to read
+         * @param bufferSize the initial buffer size
+         * @return the resource data
+         * @throws IOException if an IO error occurs
+         */
+        public static ByteBuffer ioResourceToByteBuffer(String resource, int bufferSize) throws IOException {
+            ByteBuffer buffer;
+
+            Path path = Paths.get(resource);
+            if (Files.isReadable(path)) {
+                try (SeekableByteChannel fc = Files.newByteChannel(path)) {
+                    buffer = BufferUtils.createByteBuffer((int) fc.size() + 1);
+                    while (fc.read(buffer) != -1) ;
+                }
+            } else {
+                try (
+                        InputStream source = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+                        ReadableByteChannel rbc = Channels.newChannel(source)
+                ) {
+                    buffer = BufferUtils.createByteBuffer(bufferSize);
+
+                    while (true) {
+                        int bytes = rbc.read(buffer);
+                        if (bytes == -1)
+                            break;
+                        if (buffer.remaining() == 0)
+                            buffer = resizeBuffer(buffer, buffer.capacity() * 2);
+                    }
+                }
+            }
+
+            buffer.flip();
+            return buffer;
+        }
+    }
